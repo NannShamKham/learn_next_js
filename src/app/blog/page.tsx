a4cc05f2cdd0037267350a4a7ec5159234daebbb
NannShamKham <shamkham1123@gmial.com>
2024-12-05 10:46:01 +0630

    feat:loading ui

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 export const metadata: Metadata = {
   title: "Blog",
   description: "this is blog",
@@ -7,6 +8,8 @@ export default function Blog() {
   return (
     <div>
       <h1>Blog</h1>
+      <Link href={'/blog/first'}>First</Link>
+      <Link href={'/blog/second'}>Second</Link>
     </div>
   );
 }
